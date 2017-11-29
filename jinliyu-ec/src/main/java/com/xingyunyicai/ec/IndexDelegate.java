0ab64ec3fb299f47c1709108d11ef1de22116cce
yuanjun <yuanj@xingyunyicai.com>
2017-11-29 13:02:00 +0800

    us-12：完成Banner轮播图

---
+++ jinliyu-ec/src/main/java/com/xingyunyicai/ec/IndexDelegate.java
@@ -0,0 +1,98 @@
+package com.xingyunyicai.ec;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.blankj.utilcode.util.ToastUtils;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.xingyunyicai.core.delegates.DoDoDelegate;
+import com.xingyunyicai.core.ui.image.GlideApp;
+import com.xingyunyicai.ec.bean.BannerBean;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.listener.OnBannerListener;
+import com.youth.banner.loader.ImageLoader;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+
+/**
+ * Created by YuanJun on 2017/11/29 10:28
+ */
+
+public class IndexDelegate extends DoDoDelegate {
+
+    @BindView(R2.id.banner)
+    Banner mBanner = null;
+
+    @Override
+    public Object setLayout() {
+        return R.layout.delegate_index;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
+        initBanner();
+    }
+
+    private void initBanner() {
+        final BannerBean bannerBean = initBannerBean();
+        mBanner.setImages(bannerBean.getImages())
+                .setBannerTitles(bannerBean.getTitles())
+                .setImageLoader(new ImageLoader() {
+                    @Override
+                    public void displayImage(Context context, Object path, ImageView imageView) {
+                        GlideApp.with(context)
+                                .load(path)
+                                .placeholder(R.drawable.banner_background)
+                                .apply(DEFAULT_OPTIONS)
+                                .into(imageView);
+                    }
+                })
+                .setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE_INSIDE)
+                .setIndicatorGravity(BannerConfig.RIGHT)
+                .isAutoPlay(true)
+                .setDelayTime(4000)
+                .setOnBannerListener(new OnBannerListener() {
+                    @Override
+                    public void OnBannerClick(int position) {
+                        ToastUtils.showShort("banner点击：" + position);
+                    }
+                })
+                .start();
+    }
+
+    private BannerBean initBannerBean() {
+        final BannerBean bannerBean = new BannerBean();
+        final ArrayList<String> titles = new ArrayList<>();
+        final ArrayList<String> images = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            final String title = "测试页 " + (i + 1);
+            final String image = "http://img.zcool.cn/community/01f5045663ca6c32f8754573b70db1.jpg@900w_1l_2o_100sh.jpg";
+            titles.add(title);
+            images.add(image);
+        }
+        bannerBean.setTitles(titles);
+        bannerBean.setImages(images);
+        return bannerBean;
+    }
+
+    //设置图片加载策略
+    protected static final RequestOptions DEFAULT_OPTIONS =
+            new RequestOptions()
+                    .centerCrop()
+                    .diskCacheStrategy(DiskCacheStrategy.ALL);
+//                    .transform(new RoundedCorners(6))//圆角 设置后会导致GIF丢帧
+//                    .dontAnimate();//设置后会导致GIF无法播放
+
+    @Override
+    public void onLazyInitView(@Nullable Bundle savedInstanceState) {
+        super.onLazyInitView(savedInstanceState);
+
+    }
+}
---
+++ jinliyu-ec/src/main/java/com/xingyunyicai/ec/bean/BannerBean.java
@@ -0,0 +1,29 @@
+package com.xingyunyicai.ec.bean;
+
+import java.util.List;
+
+/**
+ * Created by YuanJun on 2017/11/29 11:36
+ */
+
+public class BannerBean {
+
+    private List<String> titles;
+    private List<String> images;
+
+    public List<String> getTitles() {
+        return titles;
+    }
+
+    public void setTitles(List<String> titles) {
+        this.titles = titles;
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
+}
