08e44ec5810d52b5b8a2f5bb02379a321c77e6b6
yuanjun <yuanj@xingyunyicai.com>
2017-11-29 16:19:53 +0800

    us-12：完善Banner

--- jinliyu-ec/src/main/java/com/xingyunyicai/ec/main/index_1/IndexDelegate.java
+++ jinliyu-ec/src/main/java/com/xingyunyicai/ec/main/index_1/IndexDelegate.java
@@ -79,6 +79,12 @@ public class IndexDelegate extends BaseBottomItemDelegate {
         images.add("http://img.zcool.cn/community/010a3757a35e200000012e7ef37e76.jpg");
         titles.add("图片3");
         images.add("http://img.zcool.cn/community/014a2157ccd3ca0000018c1b7b53d1.jpg@900w_1l_2o_100sh.jpg");
+        titles.add("图片1");
+        images.add("http://img.zcool.cn/community/01f5045663ca6c32f8754573b70db1.jpg@900w_1l_2o_100sh.jpg");
+        titles.add("图片2");
+        images.add("http://img.zcool.cn/community/010a3757a35e200000012e7ef37e76.jpg");
+        titles.add("图片3");
+        images.add("http://img.zcool.cn/community/014a2157ccd3ca0000018c1b7b53d1.jpg@900w_1l_2o_100sh.jpg");
         bannerBean.setTitles(titles);
         bannerBean.setImages(images);
         return bannerBean;
